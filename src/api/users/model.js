fa73fbadea0b1ed50daf6504a18982de2d2e375f
abiodun <114649936+kaemoney0147@users.noreply.github.com>
2023-04-12 01:02:27 +0100

    Update model.js

--- src/api/users/model.js
+++ src/api/users/model.js
@@ -6,12 +6,12 @@ const url =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJIyiiNbz3VC9Afr-LqWpcw7loxjPnacfZSA&usqp=CAU";
 const usersSchema = new Schema(
   {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     username: { type: String, required: true },
     password: { type: String, required: true },
     role: {
       type: String,
-      enum: ["Carer", "Admin"],
-      default: "Carer",
       required: true,
     },
     avatar: { type: String, required: false, default: url },
